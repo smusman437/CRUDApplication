1ffe08fd8bc1cd2c800fe3e2bc5f1f48a1be4959
Muhammad Usman <50750473+smusman437@users.noreply.github.com>
2023-05-28 01:10:25 +0500

    create member model

---
+++ crud_operation/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from django.db import models
+
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
